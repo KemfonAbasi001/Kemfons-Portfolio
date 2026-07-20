8beaf869566f2a7f003e88699bd2ff4e4db26f72
Jonah, KemfonAbasi Mfon <kemfonabasijonah@gmail.com>
2026-07-20 03:42:58 +0100

    Font size adjustments

--- portfolio-web/src/pages/Process.jsx
+++ portfolio-web/src/pages/Process.jsx
@@ -143,7 +143,7 @@ function Processpage() {
               <p className="text-[1.2rem]">{s.number}</p>
             </div> */}
             <div>
-              <h1 className="uppercase text-[34px] lg:text-[64px] lg:ml-10 font-syne font-extrabold overflow-hidden">
+              <h1 className="uppercase text-[28px] lg:text-[64px] lg:ml-10 font-syne font-extrabold overflow-hidden">
                 <AnimatedWords text={s.title} step={45} />
               </h1>
               <p className="text-[1.3rem] lg:ml-60 font-medium font-inter text-[#B3B3B3] lg:w-190">
