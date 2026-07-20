ea5d1ba541833a8bfc0bff29a6bf1b0d63497a2e
Jonah, KemfonAbasi Mfon <kemfonabasijonah@gmail.com>
2026-07-20 04:00:15 +0100

    New

--- portfolio-web/src/App.jsx
+++ portfolio-web/src/App.jsx
@@ -1,23 +1,23 @@
-import Coverpage from "./pages/CoverPage"
+// import Coverpage from "./pages/CoverPage"
 // // import HeaderSec from "./components/Header"
 // // import AvailableForWorkButton from "./pages/SelectedWork"
 
-// import { Beforefooter, Footerpage } from "./components/Footer"
-// import Landingpage from "./pages/Landingpage"
-// import Processpage from "./pages/Process"
-// import Workpage from "./pages/SelectedWork"
-// import TechStack from "./pages/Techstack"
+import { Beforefooter, Footerpage } from "./components/Footer"
+import Landingpage from "./pages/Landingpage"
+import Processpage from "./pages/Process"
+import Workpage from "./pages/SelectedWork"
+import TechStack from "./pages/Techstack"
 
 function App() {
   return (
     <>
-    <Coverpage/>
-    {/* <Landingpage/>
+    {/* <Coverpage/> */}
+    <Landingpage/>
     <Workpage/>
     <TechStack/>
     <Processpage/>
     <Beforefooter/>
-    <Footerpage/> */}
+    <Footerpage/>
     {/* <HeaderSec/> */}
     {/* <AvailableForWorkButton/> */}
     
